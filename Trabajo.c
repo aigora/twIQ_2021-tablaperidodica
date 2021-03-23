9633688ac25b911279d5bfd9f59011e34644f399
davidmolano25 <47520481+davidmolano25@users.noreply.github.com>
2021-03-23 19:08:39 +0100

    Update Trabajo.c

--- Trabajo.c
+++ Trabajo.c
@@ -6,7 +6,7 @@ int main(){
 	printf("------------------------------------------\n");
 	printf("------Programa de la Tabla Periodica------\n");
 	printf("------------------------------------------\n\n\n");
-		printf("Menu de opciones.\n\n");
+        printf("Menu de opciones.\n\n");
 	
 	printf("\t1. Introduce tus credenciales.\n\n");
 	
@@ -26,20 +26,19 @@ int main(){
 	     		gets(nombre);
 	     	break;
 	     	case 2:
-			 	printf("introduzca el numero atomico del elemento.\n");//Se introduciran informacion de los elementos introducidos
-			 	scanf("%d",&num_atom);
-			break;
-			case 3:
-				printf("cual es el simbolo de la plata?.\n");  //haremos que el usuario realice un cuestionario sobre la tabla periodica.
-				gets(plata);			
-			break;
-			case 4:
-				printf("Gracias por usar nuestro programa. Hasta pronto.\n");//El usuario sale del programa
-			break;
+			 printf("introduzca el numero atomico del elemento.\n");//Se introduciran informacion de los elementos introducidos
+			 scanf("%d",&num_atom);
+		break;
+		case 3:
+			printf("cual es el simbolo de la plata?.\n");  //haremos que el usuario realice un cuestionario sobre la tabla periodica.
+			gets(plata);			
+		break;
+		case 4:
+			printf("Gracias por usar nuestro programa. Hasta pronto.\n");//El usuario sale del programa
+		break;
 			     	
-			default: printf("ERROR. Porfavor introduce un numero valido.\n");
-			
-			     	
+		default: 
+			     printf("ERROR. Porfavor introduce un numero valido.\n");	     	
 		 }
 	     
 	 }while (opcion!=4);
